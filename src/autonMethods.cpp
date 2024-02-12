b4dfcd5af50fb845dac1367bc42c1402570a8290
Science6uru <47757398+science6uru@users.noreply.github.com>
2024-02-12 14:14:30 -0600

    Update AutonMethods.cpp

--- src/autonMethods.cpp
+++ src/autonMethods.cpp
@@ -33,20 +33,25 @@ void translateInertial(int units, int voltage) {
 }
 
 void turnInertial(int degrees, int voltage) {
+  //Determine if turn should be left or right
   int direction = abs(degrees) / degrees;
+  //Calc initial speed from distance to target and voltage cap
   int speed = voltage * pow(100, 0.01 * abs(abs(inertial.get_heading) - abs(direction)) / 8) - 0.3;
 
   inertial.reset();
 
   setDrive(speed * direction, speed * -direction);
 
+  //Loop below until it reaches the target degree
   while(inertial.get_heading() > abs(degrees)) {
     pros::delay(10);
+
+    //Lower speed as it approaches target degree. not PID, but has the right effect
     speed = voltage * pow(100, 0.01 * abs(abs(inertial.get_heading) - abs(direction)) / 8) - 0.3;
     setDrive(speed * direction, speed * -direction);
   }
    
-  setDrive(-10 * direction, 10 * direction);
+  //setDrive(-10 * direction, 10 * direction);
   pros::delay(10);
 
   setDrive(0, 0);
